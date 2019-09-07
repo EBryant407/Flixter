b2d277343917d3bd45928fe89f492a73250aa7d6
Eric <EBryant407@yahoo.com>
2019-09-07 18:33:51 +0000

    Adding a Sticky Footer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.action_mailer.default_url_options = { host: 'git.heroku.com/flixter-eric-bryant.git'}
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
