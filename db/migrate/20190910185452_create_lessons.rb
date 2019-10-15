c8f2d710a45b7bace8531a036d1ecd37000dff46
Eric <EBryant407@yahoo.com>
2019-10-15 06:21:43 +0000

    Sections 1.4

--- db/migrate/20190910185452_create_lessons.rb
+++ db/migrate/20190910185452_create_lessons.rb
@@ -1,11 +1,25 @@
 class CreateLessons < ActiveRecord::Migration[5.2]
   def change
-    create_table :lessons do |t|
-      t.string :title
-      t.string :subtitle
-      t.integer :section_id
-      t.timestamps
+    create_table "lessons", force: :cascade do |t|
+      t.string "title"
+      t.string "subtitle"
+      t.integer "section_id"
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+      t.string "video"
+      t.integer "row_order"
+      t.index ["row_order"], name: "index_lessons_on_row_order"
+      t.index ["section_id"], name: "index_lessons_on_section_id"
     end
-    add_index :lessons, :section_id
+    
+  
+
+  create_table "sections", force: :cascade do |t|
+    t.string "title"
+    t.integer "course_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["course_id"], name: "index_sections_on_course_id"
   end
+
 end
