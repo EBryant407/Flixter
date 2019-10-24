8a54c96b4dcf5d695df6a9e6797d91ca2ff21b1e
Eric <EBryant407@yahoo.com>
2019-10-24 20:46:11 +0000

    Adding a Static Page 1.1

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -18,6 +18,7 @@ class EnrollmentsController < ApplicationController
        currency 'usd'
        )
     end
+  end
 
   current_user.enrollments.create(course: current_course)
   redirect_to course_path(current_course)
